a16e5dc230519d241c2ecc7c8acc67a4d40cda41
Miquel Perelló Nieto <perello.nieto@gmail.com>
2018-04-24 16:23:04 +0100

    Automatic heatmpat figure size

--- utils/visualisations.py
+++ utils/visualisations.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def df_to_heatmap(df, filename, title=None, figsize=(6,4), annotate=True):
+def df_to_heatmap(df, filename, title=None, figsize=None, annotate=True):
     ''' Exports a heatmap of the given pandas DataFrame
 
     Parameters
@@ -22,17 +22,32 @@ def df_to_heatmap(df, filename, title=None, figsize=(6,4), annotate=True):
     annotate:   bool
         If true, adds numbers inside each box
     '''
-    fig = plt.figure(figsize=figsize)
+
+    yticklabels = [''.join(row).strip() for row in df.index.values]
+    xticklabels = [''.join(col).strip() for col in df.columns.values]
+    if figsize is not None:
+        fig = plt.figure(figsize=figsize)
+    else:
+        point_inch_ratio = 72.
+        n_rows = df.shape[0]
+        font_size_pt = plt.rcParams['font.size']
+        xlabel_space_pt = max([len(xlabel) for xlabel in xticklabels])
+        fig_height_in = (xlabel_space_pt + (n_rows * (font_size_pt + 3))) / point_inch_ratio
+
+        n_cols = df.shape[1]
+        fig_width_in = df.shape[1]+4
+        ylabel_space_pt = max([len(ylabel) for ylabel in yticklabels])
+        fig_width_in = (ylabel_space_pt + (n_cols * (font_size_pt * 6))) / point_inch_ratio
+        fig = plt.figure(figsize=(fig_width_in, fig_height_in))
     ax = fig.add_subplot(111)
     if title is not None:
         ax.set_title(title)
     cax = ax.pcolor(df)
     fig.colorbar(cax)
     ax.set_yticks(np.arange(0.5, len(df.index), 1))
-    ax.set_yticklabels([' '.join(row).strip() for row in df.index.values])
+    ax.set_yticklabels(yticklabels)
     ax.set_xticks(np.arange(0.5, len(df.columns), 1))
-    ax.set_xticklabels([' '.join(col).strip() for col in df.columns.values],
-                       rotation = 45, ha="right")
+    ax.set_xticklabels(xticklabels, rotation = 45, ha="right")
 
     middle_value = (df.values.max() + df.values.min())/2.0
     print(middle_value)
@@ -45,5 +60,9 @@ def df_to_heatmap(df, filename, title=None, figsize=(6,4), annotate=True):
                          verticalalignment='center',
                          color=color
                          )
-    fig.tight_layout()
+    try:
+        fig.tight_layout()
+    except ValueError as e:
+        print(e)
+        print('Canceling tight_layout for figure {}'.format(filename))
     fig.savefig(filename)
