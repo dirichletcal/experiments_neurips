e8471975b019550fc572196073d45264f58526a5
Telmo Filho <tmfilho@gmail.com>
2018-04-25 08:43:51 -0300

    Adapts calibration.py to multiclass

--- calib/models/calibration.py
+++ calib/models/calibration.py
@@ -24,14 +24,7 @@ from dirichlet import DirichletCalibrator
 
 class CalibratedModel(BaseEstimator, ClassifierMixin):
     def __init__(self, base_estimator=None, method=None, score_type=None):
-        if method is None:
-            self.method = method
-            self.platts_trick = False
-        else:
-            temp = method.split('-')
-            self.platts_trick = (len(temp) == 2)
-            self.method = temp[0]
-
+        self.method = method
         self.base_estimator = base_estimator
         self.score_type = score_type
 
@@ -40,35 +33,36 @@ class CalibratedModel(BaseEstimator, ClassifierMixin):
         self.score_type = score_type
 
     def _preproc(self, X):
-        if self.score_type is None:
-            if hasattr(self.base_estimator, "decision_function"):
-                df = self.base_estimator.decision_function(X)
-                if df.ndim == 1:
-                    df = df[:, np.newaxis]
-            elif hasattr(self.base_estimator, "predict_proba"):
-                df = self.base_estimator.predict_proba(X)
-                df = df[:, 1]
-            else:
-                raise RuntimeError('classifier has no decision_function or '
-                                   'predict_proba method.')
-        else:
-            if self.score_type == "sigmoid":
-                df = self.base_estimator.decision_function(X)
-                df = expit(df)
-                if df.ndim == 1:
-                    df = df[:, np.newaxis]
-            else:
-                if hasattr(self.base_estimator, self.score_type):
-                    df = getattr(self.base_estimator, self.score_type)(X)
-                    if self.score_type == "decision_function":
-                        if df.ndim == 1:
-                            df = df[:, np.newaxis]
-                    elif self.score_type == "predict_proba":
-                        df = df[:, 1:]
-                else:
-                    raise RuntimeError('classifier has no ' + self.score_type
-                                       + 'method.')
-        return df.reshape(-1)
+        # if self.score_type is None:
+        #     if hasattr(self.base_estimator, "decision_function"):
+        #         df = self.base_estimator.decision_function(X)
+        #         if df.ndim == 1:
+        #             df = df[:, np.newaxis]
+        #     elif hasattr(self.base_estimator, "predict_proba"):
+        df = self.base_estimator.predict_proba(X)
+        #         df = df[:, 1]
+        #     else:
+        #         raise RuntimeError('classifier has no decision_function or '
+        #                            'predict_proba method.')
+        # else:
+        #     if self.score_type == "sigmoid":
+        #         df = self.base_estimator.decision_function(X)
+        #         df = expit(df)
+        #         if df.ndim == 1:
+        #             df = df[:, np.newaxis]
+        #     else:
+        #         if hasattr(self.base_estimator, self.score_type):
+        #             df = getattr(self.base_estimator, self.score_type)(X)
+        #             if self.score_type == "decision_function":
+        #                 if df.ndim == 1:
+        #                     df = df[:, np.newaxis]
+        #             elif self.score_type == "predict_proba":
+        #                 df = df[:, 1:]
+        #         else:
+        #             raise RuntimeError('classifier has no ' + self.score_type
+        #                                + 'method.')
+        # return df.reshape(-1)
+        return df
 
     def fit(self, X, y, sample_weight=None):
         """Fit the calibrated model
@@ -78,7 +72,7 @@ class CalibratedModel(BaseEstimator, ClassifierMixin):
         X : array-like, shape (n_samples, n_features)
             Training data.
 
-        y : array-like, shape (n_samples,)
+        y : array-like, shape (n_samples, n_classes)
             Target values.
 
         sample_weight : array-like, shape = [n_samples] or None
@@ -91,88 +85,35 @@ class CalibratedModel(BaseEstimator, ClassifierMixin):
         """
 
         X, y = check_X_y(X, y, accept_sparse=['csc', 'csr', 'coo'],
-                         force_all_finite=False)
+                         multi_output=True)
         X, y = indexable(X, y)
 
         df = self._preproc(X)
 
-        weights = None
-        if self.platts_trick:
-            # Bayesian priors (see Platt end of section 2.2)
-            prior0 = float(np.sum(y <= 0))
-            prior1 = y.shape[0] - prior0
+        # weights = None
+        # if self.platts_trick:
+        #     # Bayesian priors (see Platt end of section 2.2)
+        #     prior0 = float(np.sum(y <= 0))
+        #     prior1 = y.shape[0] - prior0
 
-            weights = np.zeros_like(y).astype(float)
-            weights[y > 0] = (prior1 + 1.) / (prior1 + 2.)
-            weights[y <= 0] = 1. / (prior0 + 2.)
-            y = np.append(np.ones_like(y), np.zeros_like(y))
-            weights = np.append(weights, 1.0 - weights)
-            df = np.append(df, df)
+        #     weights = np.zeros_like(y).astype(float)
+        #     weights[y > 0] = (prior1 + 1.) / (prior1 + 2.)
+        #     weights[y <= 0] = 1. / (prior0 + 2.)
+        #     y = np.append(np.ones_like(y), np.zeros_like(y))
+        #     weights = np.append(weights, 1.0 - weights)
+        #     df = np.append(df, df)
 
         if self.method is None:
             self.calibrator = _DummyCalibration()
-        elif self.method == 'isotonic':
-            self.calibrator = IsotonicRegression(out_of_bounds='clip')
-        elif self.method == 'sksigmoid':
-            self.calibrator = sk_sigmoid()
-        elif self.method == 'sksigmoid_notrick':
-            self.calibrator = sk_sigmoid_notrick()
-        elif self.method == 'sigmoid':
-            self.calibrator = _SigmoidCalibration()
-        elif self.method == 'beta':
-            self.calibrator = BetaCalibration(parameters="abm")
-        elif self.method == 'beta_am':
-            self.calibrator = BetaCalibration(parameters="am")
-        elif self.method == 'beta_ab':
-            self.calibrator = BetaCalibration(parameters="ab")
+        elif self.method == 'multinomial':
+            self.calibrator = _MultinomialCalibrator()
         elif self.method == 'dirichlet_full':
             self.calibrator = DirichletCalibrator()
         else:
-            raise ValueError('method should be None, "sigmoid", '
-                             '"isotonic", "beta", "beta_am" or "beta_ab". '
+            raise ValueError('method should be None, "multinomial", '
+                             'or "dirichlet_full". '
                              'Got %s.' % self.method)
-        self.calibrator.fit(df, y, weights)
-        # if self.method == 'beta':
-        #     df_pos = df[y == 1]
-        #     df_neg = df[y == 0]
-        #
-        #     # alpha_pos_nll, beta_pos_nll = fit_beta_nll(df_pos)
-        #     # alpha_neg_nll, beta_neg_nll = fit_beta_nll(df_neg)
-        #
-        #     # a_nll = alpha_pos_nll - alpha_neg_nll
-        #     # b_nll = beta_neg_nll - beta_pos_nll
-        #     # m_nll = fit_beta_midpoint(alpha_pos_nll, beta_pos_nll,
-        #     #                           alpha_neg_nll, beta_neg_nll)
-        #
-        #     alpha_pos_mmt, beta_pos_mmt = fit_beta_moments(df_pos)
-        #     alpha_neg_mmt, beta_neg_mmt = fit_beta_moments(df_neg)
-        #
-        #     a_mmt = alpha_pos_mmt - alpha_neg_mmt
-        #     if a_mmt < 0 or np.isnan(a_mmt):
-        #         a_mmt = 0
-        #     b_mmt = beta_neg_mmt - beta_pos_mmt
-        #     if b_mmt < 0 or np.isnan(b_mmt):
-        #         b_mmt = 0
-        #     prior_pos = len(df_pos) / len(df)
-        #     prior_neg = len(df_neg) / len(df)
-        #     m_mmt = fit_beta_midpoint(prior_pos, alpha_pos_mmt, beta_pos_mmt,
-        #                               prior_neg, alpha_neg_mmt, beta_neg_mmt)
-        #     map = self.calibrator.calibrator_.map_
-        # #     if a_mmt > 4 and map[0] < 2:
-        # #         print [a_mmt, map[0]]
-        # #         print [b_mmt, map[1]]
-        # #         print [m_mmt, map[2]]
-        # #         exit()
-        # #     if b_mmt > 4 and map[1] < 2:
-        # #         print [a_mmt, map[0]]
-        # #         print [b_mmt, map[1]]
-        # #         print [m_mmt, map[2]]
-        # #         exit()
-        #     self.a = [a_mmt, map[0]]
-        #     self.b = [b_mmt, map[1]]
-        #     self.m = [m_mmt, map[2]]
-        #     self.df_pos = df_pos
-        #     self.df_neg = df_neg
+        self.calibrator.fit(df, y, sample_weight)
         return self
 
     def predict_proba(self, X):
@@ -192,32 +133,30 @@ class CalibratedModel(BaseEstimator, ClassifierMixin):
             The predicted probas. Can be exact zeros.
         """
 
-        proba = np.zeros((X.shape[0], 2))
-
         df = self._preproc(X)
 
         # FIXME Should this be predict or predict_proba?
-        if hasattr(self.calibrator, "predict_proba"):
-            prediction = self.calibrator.predict_proba(df)
-        elif hasattr(self.calibrator, "predict"):
-            prediction = self.calibrator.predict(df)
-        else:
-	        raise RuntimeError('classifier has no predict_proba or ' +
-	                           'predict method.')
+        # if hasattr(self.calibrator, "predict_proba"):
+        predictions = self.calibrator.predict_proba(df)
+        # elif hasattr(self.calibrator, "predict"):
+        #     prediction = self.calibrator.predict(df)
+        # else:
+	    #     raise RuntimeError('classifier has no predict_proba or ' +
+	    #                        'predict method.')
 
-        if len(prediction.shape) == 1:
-            proba[:, 1] = prediction
-            proba[:, 0] = 1. - proba[:, 1]
-        elif len(prediction.shape) == 2:
-            proba = prediction
-        else:
-            raise RuntimeError('The prediction is expected to be binary ' +
-                               'but multiple classes returned from calibrator')
+        # if len(prediction.shape) == 1:
+        #     proba[:, 1] = prediction
+        #     proba[:, 0] = 1. - proba[:, 1]
+        # elif len(prediction.shape) == 2:
+        #     proba = prediction
+        # else:
+        #     raise RuntimeError('The prediction is expected to be binary ' +
+        #                        'but multiple classes returned from calibrator')
 
         # Deal with cases where the predicted probability minimally exceeds 1.0
-        proba[(1.0 < proba) & (proba <= 1.0 + 1e-5)] = 1.0
+        # proba[(1.0 < proba) & (proba <= 1.0 + 1e-5)] = 1.0
 
-        return proba
+        return predictions
 
     def predict(self, X):
         """Predict the target of new samples. Can be different from the
@@ -237,7 +176,7 @@ class CalibratedModel(BaseEstimator, ClassifierMixin):
         return np.argmax(self.predict_proba(X), axis=1)
 
 
-class _SigmoidCalibration(BaseEstimator, RegressorMixin):
+class _MultinomialCalibrator(BaseEstimator, RegressorMixin):
     """Sigmoid regression model.
 
     Attributes
@@ -267,15 +206,15 @@ class _SigmoidCalibration(BaseEstimator, RegressorMixin):
         self : object
             Returns an instance of self.
         """
-        X = column_or_1d(X)
-        y = column_or_1d(y)
         X, y = indexable(X, y)
-        self.lr = LogisticRegression(C=99999999999)
-        self.lr.fit(X.reshape(-1, 1), y, sample_weight=sample_weight)
+        self.lr = LogisticRegression(
+            C=99999999999,
+            multi_class='multinomial', solver='saga'
+            ).fit(X, y, sample_weight)
         return self
 
-    def predict(self, T):
-        """Predict new data by linear interpolation.
+    def predict_proba(self, T):
+        """Predict new data.
 
         Parameters
         ----------
@@ -284,11 +223,10 @@ class _SigmoidCalibration(BaseEstimator, RegressorMixin):
 
         Returns
         -------
-        T_ : array, shape (n_samples,)
+        T_ : array, shape (n_samples, n_classes)
             The predicted data.
         """
-        T = column_or_1d(T)
-        return self.lr.predict_proba(T.reshape(-1, 1))[:, 1]
+        return self.lr.predict_proba(T)
 
 
 class _DummyCalibration(BaseEstimator, RegressorMixin):
@@ -319,7 +257,7 @@ class _DummyCalibration(BaseEstimator, RegressorMixin):
         """
         return self
 
-    def predict(self, T):
+    def predict_proba(self, T):
         """Return the probabilities of the base classifier.
 
         Parameters
@@ -329,57 +267,7 @@ class _DummyCalibration(BaseEstimator, RegressorMixin):
 
         Returns
         -------
-        T_ : array, shape (n_samples,)
+        T_ : array, shape (n_samples, n_classes)
             The predicted data.
         """
         return T
-
-
-class _BetaTestedCalibration(BaseEstimator, RegressorMixin):
-    """Dummy regression model. The purpose of this class is to give
-    the CalibratedClassifierCV class the option to just return the
-    probabilities of the base classifier.
-
-
-    """
-    def fit(self, X, y, sample_weight=None):
-        """Fit beta calibration only if the classifier is considered 
-        uncalibrated.
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples,)
-            Training data.
-
-        y : array-like, shape (n_samples,)
-            Training target.
-
-        sample_weight : array-like, shape = [n_samples] or None
-            Sample weights. If None, then samples are equally weighted.
-
-        Returns
-        -------
-        self : object
-            Returns an instance of self.
-        """
-        self._calibrator = BetaCalibration(parameters="abm").fit(X, y)
-        test = beta_test(self._calibrator.calibrator_.map_,
-                         test_type="adev", scores=X)
-        if test["p-value"] >= 0.5:
-            self._calibrator = _DummyCalibration().fit(X, y)
-        return self
-
-    def predict(self, T):
-        """Return the probabilities of the base classifier.
-
-        Parameters
-        ----------
-        T : array-like, shape (n_samples,)
-            Data to predict from.
-
-        Returns
-        -------
-        T_ : array, shape (n_samples,)
-            The predicted data.
-        """
-        return self._calibrator.predict(T)
