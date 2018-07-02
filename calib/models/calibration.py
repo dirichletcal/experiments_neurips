6afdec9c84e56b47ff110b510689c351c25471d0
Miquel Perelló Nieto <perello.nieto@gmail.com>
2018-07-02 12:31:49 +0100

    Updated documentation of DummyCalibrator

--- calib/models/calibration.py
+++ calib/models/calibration.py
@@ -235,7 +235,7 @@ class _DummyCalibration(BaseEstimator, RegressorMixin):
 
         Parameters
         ----------
-        T : array-like, shape (n_samples,)
+        T : array-like, shape (n_samples, n_classes)
             Data to predict from.
 
         Returns
