e15e8632b1b33b74cf91c2c5a308f373f31583c3
Miquel Perelló Nieto <perello.nieto@gmail.com>
2018-07-12 14:22:48 +0100

    Parallelise datasets and methods in Slurm

--- add_queue_dirichlet.sh
+++ add_queue_dirichlet.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=dirichlet_job
-#SBATCH --partition=cpu
+#SBATCH --partition=veryshort
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=28
-#SBATCH --time=3-00:00:00
+#SBATCH --time=0-06:00:00
 ##SBATCH --mem=100M
-#SBATCH --array=0-5
+### A total of 156 runs
+#SBATCH --array=100-155
 #SBATCH --exclusive
 
 # Activate the virtual environment
@@ -35,14 +36,51 @@ declare -a classifier_names=(
     'svm'
     )
 
-classifier=${classifier_names[$SLURM_ARRAY_TASK_ID]}
+declare -a dataset_names=(
+    'optdigits'
+    'libras-movement'
+    'pendigits'
+    'dermatology'
+    'cleveland'
+    'landsat-satellite'
+    'yeast'
+    'zoo'
+    'vehicle'
+    'shuttle'
+    'waveform-5000'
+    'vowel'
+    'ecoli'
+    'page-blocks'
+    'autos'
+    'abalone'
+    'letter'
+    'segment'
+    'mfeat-morphological'
+    'iris'
+    'glass'
+    'car'
+    'balance-scale'
+    'mfeat-karhunen'
+    'mfeat-zernike'
+    'flare'
+    )
+
+n_classifiers=${#classifier_names[@]}
+n_datasets=${#dataset_names[@]}
+
+classifier_id=$((SLURM_ARRAY_TASK_ID%n_classifiers))
+dataset_id=$((SLURM_ARRAY_TASK_ID/n_classifiers))
+
+classifier=${classifier_names[$classifier_id]}
+dataset=${dataset_names[$dataset_id]}
 
 hosts=$(srun bash -c hostname)
 echo "Hosts are ${hosts}"
 
+#datasets='abalone'
 datasets='datasets_non_binary'
 output_path='results'`date +"_%d_%m_%Y_"`${datasets}
 
 time srun python -m scoop --host ${hosts} -v main.py \
     --classifier ${classifier} --output-path ${output_path} \
-    --datasets ${datasets}
+    --datasets ${dataset} --seed 42
