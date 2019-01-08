0ac22b58e9f75642227ff8e2baadd6a90e5f7acf
Miquel Perelló Nieto <perello.nieto@gmail.com>
2019-01-08 11:48:24 +0000

    Added test set and additional confusion matrices

--- notebooks/utils/visualisations.py
+++ notebooks/utils/visualisations.py
@@ -102,35 +102,46 @@ def plot_reliability_diagram(score, labels, linspace, scores_set, legend_set,
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
-                          cmap=plt.cm.Blues):
+                          cmap=plt.cm.Blues,
+                          fig=None, ax=None):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
+    if fig is None:
+        fig = plt.figure()
+
+    if ax is None:
+        ax = fig.add_subplot(111)
+
+    if title is not None:
+        ax.set_title(title)
+
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
+    cs = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    fig.colorbar(cs)
     tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+    ax.set_xticks(tick_marks)
+    ax.set_xticklabels(classes, rotation=45)
+    ax.set_yticks(tick_marks)
+    ax.set_yticklabels(classes)
 
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+        ax.text(j, i, format(cm[i, j], fmt),
+                horizontalalignment="center",
+                color="white" if cm[i, j] > thresh else "black")
 
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
+    ax.set_ylabel('True label')
+    ax.set_xlabel('Predicted label')
+    fig.tight_layout()
 
 def plot_weight_matrix(weights, bias, classes,
                           title='Weight matrix',
-                          cmap=plt.cm.Blues):
+                          cmap=plt.cm.Greens):
     """
     This function prints and plots the weight matrix.
     """
