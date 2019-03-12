edafef2aad6673bc9e94019291bce12a0b3f0032
Miquel Perelló Nieto <perello.nieto@gmail.com>
2019-03-12 20:07:22 +0000

    First working test for ECE

--- calib/tests/utils/test_functions.py
+++ calib/tests/utils/test_functions.py
@@ -1,23 +1,28 @@
 import unittest
 import numpy as np
-from . import get_simple_ternary_example
 from calib.utils.functions import binary_ECE, classwise_ECE, full_ECE
 
-from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import label_binarize
 
 
 class TestFunctions(unittest.TestCase):
     def test_binary_ece(self):
-        S, Y = get_simple_ternary_example()
-        print(binary_ECE(S, Y))
+        S = np.array([.6, .6, .6, .6, .6, .6, .6, .6, .6, .6])
+        y = np.array([1, 1, 1, 1, 1, 1, 0, 0, 0, 0])
+        ece = binary_ECE(S, y)
+        self.assertAlmostEqual(ece, 0)
 
     def test_classwise_ece(self):
-        S, Y = get_simple_ternary_example()
-        print(classwise_ECE(S, Y))
+        S = np.array([[0.6, 0.2, 0.2]]*10)
+        Y = label_binarize([0, 0, 0, 0, 0, 0, 1, 1, 2, 2], range(3))
+        ece = classwise_ECE(S, Y)
+        self.assertAlmostEqual(ece, 0)
 
     def test_full_ece(self):
-        S, Y = get_simple_ternary_example()
-        print(full_ECE(S, Y))
+        S = np.array([[0.6, 0.2, 0.2]]*10)
+        Y = label_binarize([0, 0, 0, 0, 0, 0, 1, 1, 2, 2], range(3))
+        ece = full_ECE(S, Y)
+        self.assertAlmostEqual(ece, 0)
 
 
 def main():
